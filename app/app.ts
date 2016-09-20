1490a75db6fcbeb1b862165e6156ea5b2696a51f
thebossza101 <rujiroj2537@gmail.com>
2016-09-20 15:27:31 +0700

    add push
    PushNotification

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,25 @@ import { StatusBar } from 'ionic-native';
 
 import { LoginPage } from './pages/login/login';
 
+import {provideCloud, CloudSettings,  Push, PushToken} from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '0b3a88f5',
+  },
+  'push': {
+    'sender_id': '493836334159',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
+};
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -11,13 +30,26 @@ import { LoginPage } from './pages/login/login';
 export class MyApp {
   rootPage: any = LoginPage;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform,public push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+     this.push.register().then((t: PushToken) => {
+    return this.push.saveToken(t);
+  }).then((t: PushToken) => {
+    console.log('Token saved:', t.token);
+  });
+
+  this.push.rx.notification()
+  .subscribe((msg) => {
+    alert(msg.title + ': ' + msg.text);
+
+  });
+  
+
       StatusBar.styleDefault();
     });
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp,[provideCloud(cloudSettings)]);
